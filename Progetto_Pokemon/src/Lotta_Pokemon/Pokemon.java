428f7c027307036bf6d9b5b0146560e53ae13e3d
DonnieRouge7 <massimo.donnini2001@gmail.com>
2025-03-14 16:38:26 +0100

    daiii che ci siamooo

--- Progetto_Pokemon/src/Lotta_Pokemon/Pokemon.java
+++ Progetto_Pokemon/src/Lotta_Pokemon/Pokemon.java
@@ -7,72 +7,72 @@ import java.util.List;
  * Contiene metodi per l'uso delle mosse e la gestione del livello.
  * 
  * @author Massimo Donnini
- * @numero_matricola 1965076
+ * 
  */
 public class Pokemon {
 	
-	/** Codice identificativo del Pokémon */
+	/** Codice identificativo del Pokemon */
 	private String codice;
 	
-	/** Tipo primario del Pokémon (rappresentato da un codice numerico) */
+	/** Tipo primario del Pokemon (rappresentato da un codice numerico) */
 	private int tipo1;
 	
-	/** Tipo secondario del Pokémon, -1 se non ha un secondo tipo */
+	/** Tipo secondario del Pokemon, -1 se non ha un secondo tipo */
 	private int tipo2;
 	
-	/** Nome del Pokémon */
+	/** Nome del Pokemon */
 	private String nome;
 	
-	/** Esperienza attuale del Pokémon */
+	/** Esperienza attuale del Pokemon */
 	private int xp;
 	
-	/** Livello attuale del Pokémon */
+	/** Livello attuale del Pokemon */
 	private int livello;
 	
-	/** Punti vita attuali del Pokémon */
+	/** Punti vita attuali del Pokemon */
 	private double hp;
 	
-	/** Punti vita massimi del Pokémon */
+	/** Punti vita massimi del Pokemon */
 	private double hpMax;
 	
-	/** Attacco fisico del Pokémon */
+	/** Attacco fisico del Pokemon */
 	private int attacco;
 	
-	/** Difesa fisica del Pokémon */
+	/** Difesa fisica del Pokemon */
 	private int difesa;
 	
-	/** Attacco speciale del Pokémon */
+	/** Attacco speciale del Pokemon */
 	private int attaccoSpeciale;
 	
-	/** Difesa speciale del Pokémon */
+	/** Difesa speciale del Pokemon */
 	private int difesaSpeciale;
 	
 	/** Velocità del Pokémon */
 	private int velocita;
 	
-	/** Percentuale di elusione del Pokémon */
+	/** Percentuale di elusione del Pokemon */
 	private double elusione = 30;
 	
-	/** Lista delle mosse disponibili del Pokémon */
+	/** Lista delle mosse disponibili del Pokemon */
 	private List<Mossa> mosse;
 
 	/**
 	 * Costruttore della classe Pokemon.
 	 * 
-	 * @param codice Identificativo del Pokémon.
-	 * @param tipo1 Tipo primario del Pokémon.
-	 * @param tipo2 Tipo secondario del Pokémon (-1 se non esiste).
-	 * @param nome Nome del Pokémon.
-	 * @param xp Esperienza del Pokémon.
-	 * @param livello Livello attuale del Pokémon.
+	 * @param codice Identificativo del Pokemon.
+	 * @param tipo1 Tipo primario del Pokemon.
+	 * @param tipo2 Tipo secondario del Pokemon (-1 se non esiste).
+	 * @param nome Nome del Pokemon.
+	 * @param xp Esperienza del Pokemon.
+	 * @param livello Livello attuale del Pokemon.
 	 * @param hp Punti vita attuali.
 	 * @param hpMax Punti vita massimi.
-	 * @param attacco Attacco fisico del Pokémon.
-	 * @param difesa Difesa fisica del Pokémon.
-	 * @param attaccoSpeciale Attacco speciale del Pokémon.
-	 * @param difesaSpeciale Difesa speciale del Pokémon.
-	 * @param velocita Velocità del Pokémon.
-	 * @param elusione Percentuale di elusione del Pokémon.
+	 * @param attacco Attacco fisico del Pokemon.
+	 * @param difesa Difesa fisica del Pokemon.
+	 * @param attaccoSpeciale Attacco speciale del Pokemon.
+	 * @param difesaSpeciale Difesa speciale del Pokemon.
+	 * @param velocita Velocità del Pokemon.
+	 * @param elusione Percentuale di elusione del Pokemon.
 	 */
 	
 	public Pokemon(String codice, int tipo1, int tipo2, String nome, int xp, int livello, double hp, double hpMax, int attacco, int difesa, int attaccoSpeciale, int difesaSpeciale, int velocita, double elusione) {
@@ -92,6 +92,189 @@ public class Pokemon {
 		this.elusione = 30;
 	}
 	
+	// Getters e Setters 
+	
+		/**
+		 * Restituisce il codice identificativo del Pokemon.
+		 * @return Il codice del Pokémon.
+		 */
+		public String getCodice() { return codice; }
+
+		/**
+		 * Imposta il codice identificativo del Pokemon.
+		 * @param codice Il codice da assegnare.
+		 */
+		public void setCodice(String codice) { this.codice = codice; }
+
+		/**
+		 * Restituisce il tipo primario del Pokemon.
+		 * @return Il codice del tipo primario.
+		 */
+		public int getTipo1() { return tipo1; }
+
+		/**
+		 * Imposta il tipo primario del Pokemon.
+		 * @param tipo1 Il codice del tipo primario.
+		 */
+		public void setTipo1(int tipo1) { this.tipo1 = tipo1; }
+
+		/**
+		 * Restituisce il tipo secondario del Pokemon.
+		 * @return Il codice del tipo secondario o -1 se non presente.
+		 */
+		public int getTipo2() { return tipo2; }
+
+		/**
+		 * Imposta il tipo secondario del Pokemon.
+		 * @param tipo2 Il codice del tipo secondario.
+		 */
+		public void setTipo2(int tipo2) { this.tipo2 = tipo2; }
+
+		/**
+		 * Restituisce il nome del Pokemon.
+		 * @return Il nome del Pokemon.
+		 */
+		public String getNome() { return nome; }
+
+		/**
+		 * Imposta il nome del Pokemon.
+		 * @param nome Il nome da assegnare.
+		 */
+		public void setNome(String nome) { this.nome = nome; }
+
+		/**
+		 * Restituisce il livello attuale del Pokemon.
+		 * @return Il livello attuale.
+		 */
+		public int getLivello() { return livello; }
+
+		/**
+		 * Imposta il livello del Pokemon.
+		 * @param livello Il livello da assegnare.
+		 */
+		public void setLivello(int livello) { this.livello = livello; }
+
+		/**
+		 * Restituisce i punti vita attuali del Pokemon.
+		 * @return Gli HP attuali.
+		 */
+		public double getHp() { return hp; }
+
+		/**
+		 * Imposta i punti vita attuali del Pokemon.
+		 * @param hp Gli HP da assegnare.
+		 */
+		public void setHp(double hp) { this.hp = hp; }
+
+		/**
+		 * Restituisce i punti vita massimi del Pokemon.
+		 * @return Gli HP massimi.
+		 */
+		public double getHpMax() { return hpMax; }
+
+		/**
+		 * Imposta i punti vita massimi del Pokemon.
+		 * @param hpMax Gli HP massimi da assegnare.
+		 */
+		public void setHpMax(double hpMax) { this.hpMax = hpMax; }
+
+		/**
+		 * Restituisce il valore di attacco fisico del Pokemon.
+		 * @return Il valore dell'attacco fisico.
+		 */
+		public int getAttacco() { return attacco; }
+
+		/**
+		 * Imposta il valore di attacco fisico del Pokemon.
+		 * @param attacco Il valore dell'attacco fisico.
+		 */
+		public void setAttacco(int attacco) { this.attacco = attacco; }
+
+		/**
+		 * Restituisce il valore della difesa fisica del Pokemon.
+		 * @return Il valore della difesa.
+		 */
+		public int getDifesa() { return difesa; }
+
+		/**
+		 * Imposta il valore della difesa fisica del Pokemon.
+		 * @param difesa Il valore della difesa.
+		 */
+		public void setDifesa(int difesa) { this.difesa = difesa; }
+
+		/**
+		 * Restituisce il valore dell'attacco speciale del Pokemon.
+		 * @return Il valore dell'attacco speciale.
+		 */
+		public int getAttaccoSpeciale() { return attaccoSpeciale; }
+
+		/**
+		 * Imposta il valore dell'attacco speciale del Pokemon.
+		 * @param attaccoSpeciale Il valore dell'attacco speciale.
+		 */
+		public void setAttaccoSpeciale(int attaccoSpeciale) { this.attaccoSpeciale = attaccoSpeciale; }
+		
+		/**
+		 * Restituisce il valore della difesa speciale del Pokemon.
+		 * @return Il valore della difesa speciale.
+		 */
+		public int getDifesaSpeciale() { return difesaSpeciale; }
+
+		/**
+		 * Imposta il valore della difesa speciale del Pokemon.
+		 * @param difesaSpeciale Il valore della difesa speciale.
+		 */
+		public void setDifesaSpeciale(int difesaSpeciale) { this.difesaSpeciale = difesaSpeciale; }
+		
+		/**
+		 * Restituisce il valore della velocita del Pokemon.
+		 * @return Il valore della velocita.
+		 */
+		public int getVelocita() { return velocita; }
+
+		/**
+		 * Imposta il valore della velocita del Pokemon.
+		 * @param velocita Il valore della velocita.
+		 */
+		public void setVelocita(int velocita) { this.velocita = velocita; }
+		
+		/**
+		 * Restituisce il valore dell'elusione del Pokemon.
+		 * @return Il valore dell'elusione.
+		 */
+		public double getElusione() { return elusione; }
+
+		/**
+		 * Imposta il valore dell'elusione del Pokemon.
+		 * @param elusione Il valore dell'elusione.
+		 */
+		public void setElusione(double elusione) { this.elusione = elusione; }
+		
+		/**
+		 * Restituisce il valore degli xp del Pokemon.
+		 * @return Il valore degli xp.
+		 */
+		public int getXp() { return xp; }
+			
+		/**
+		 * Imposta il valore degli xp del Pokemon.
+		 * @param xp Il valore degli xp.
+		 */
+		public void setXp(int xp) { this.xp = xp; }
+			
+		/**
+		 * Restituisce la lista delle mosse del Pokemon.
+		 * @return La lista delle mosse.
+		 */
+		public List<Mossa> getMosse() { return mosse; }
+			
+		/**
+		 * Imposta la lista delle mosse del Pokemon.
+		 * @param mosse la lista delle mosse.
+		 */
+		public void setMosse(List<Mossa> mosse) { this.mosse = mosse; }
+			
+	
 	@Override
 	public String toString() {
 		return "Pokemon [codice=" + codice + ", tipo1=" + tipo1 + ", tipo2=" + tipo2 + ", nome=" + nome + ", xp=" + xp
@@ -102,10 +285,10 @@ public class Pokemon {
 	}
 	
 	/**
-	 * Verifica se il Pokémon può salire di livello in base all'XP.
+	 * Verifica se il Pokemon può salire di livello in base all'XP.
 	 * Se il Pokémon sale di livello, migliora le sue statistiche.
 	 * 
-	 * @return true se il Pokémon è salito di livello, false altrimenti.
+	 * @return true se il Pokemon è salito di livello, false altrimenti.
 	 */
 	public boolean saliDiLivello() {
 		if (xp >= getLivello() * 10) {
@@ -126,7 +309,7 @@ public class Pokemon {
 	 * Permette al Pokémon di usare una mossa su un bersaglio.
 	 * Se la mossa è di attacco, infligge danno; se è di stato, modifica statistiche.
 	 * 
-	 * @param bersaglio Il Pokémon bersaglio dell'attacco.
+	 * @param bersaglio Il Pokemon bersaglio dell'attacco.
 	 * @param mossa La mossa da utilizzare.
 	 */
 	public void usaMossa(Pokemon bersaglio, Mossa mossa) {
@@ -141,9 +324,9 @@ public class Pokemon {
 	}
 	
 	/**
-	 * Controlla se il Pokémon è esausto (ha 0 HP).
+	 * Controlla se il Pokemon è esausto (ha 0 HP).
 	 * 
-	 * @return true se il Pokémon è esausto, false altrimenti.
+	 * @return true se il Pokemon è esausto, false altrimenti.
 	 */
 	public boolean esausto() {
 		if (getHp() <= 0) {
@@ -153,49 +336,6 @@ public class Pokemon {
 		return false;
 	}
 
-	// Getters e Setters 
-	public String getCodice() { return codice; }
-	public void setCodice(String codice) { this.codice = codice; }
 	
-	public int getTipo1() { return tipo1; }
-	public void setTipo1(int tipo1) { this.tipo1 = tipo1; }
-	
-	public int getTipo2() { return tipo2; }
-	public void setTipo2(int tipo2) { this.tipo2 = tipo2; }
-	
-	public String getNome() { return nome; }
-	public void setNome(String nome) { this.nome = nome; }
-	
-	public int getLivello() { return livello; }
-	public void setLivello(int livello) { this.livello = livello; }
-	
-	public double getHp() { return hp; }
-	public void setHp(double hp) { this.hp = hp; }
-	
-	public double getHpMax() { return hpMax; }
-	public void setHpMax(double hpMax) { this.hpMax = hpMax; }
-	
-	public int getAttacco() { return attacco; }
-	public void setAttacco(int attacco) { this.attacco = attacco; }
-	
-	public int getDifesa() { return difesa; }
-	public void setDifesa(int difesa) { this.difesa = difesa; }
-	
-	public int getAttaccoSpeciale() { return attaccoSpeciale; }
-	public void setAttaccoSpeciale(int attaccoSpeciale) { this.attaccoSpeciale = attaccoSpeciale; }
-	
-	public int getDifesaSpeciale() { return difesaSpeciale; }
-	public void setDifesaSpeciale(int difesaSpeciale) { this.difesaSpeciale = difesaSpeciale; }
-	
-	public int getVelocita() { return velocita; }
-	public void setVelocita(int velocita) { this.velocita = velocita; }
-	
-	public double getElusione() { return elusione; }
-	public void setElusione(double elusione) { this.elusione = elusione; }
-	
-	public List<Mossa> getMosse() { return mosse; }
-	public void setMosse(List<Mossa> mosse) { this.mosse = mosse; }
-	
-	public int getXp() { return xp; }
-	public void setXp(int xp) { this.xp = xp; }
+
 }
